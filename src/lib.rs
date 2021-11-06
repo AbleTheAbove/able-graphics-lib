f71ff3c6ac791097fd14f4b8165fdeabf159fd5b
Able <abl3theabove@gmail.com>
2021-11-06 05:25:18 -0500

    trait fix up

--- src/lib.rs
+++ src/lib.rs
@@ -17,17 +17,17 @@ pub type Point = (GCoord, GCoord);
 pub struct FrameBuffer;
 
 pub trait Graphics {
-    fn put_line(coords_start: Point, coords_end: Point, thickness: u32, color: RGB);
-    fn put_rect(coords_start: Point, coords_end: Point, color: RGB);
-    fn put_circle(coords: Point, radius: u32);
-    fn put_pixel(coords: Point, color: RGB);
+    fn put_line(&self, coords_start: Point, coords_end: Point, thickness: u32, color: RGB);
+    fn put_rect(&self, coords_start: Point, coords_end: Point, color: RGB);
+    fn put_circle(&self, coords: Point, radius: u32);
+    fn put_pixel(&self, coords: Point, color: RGB);
 
-    fn paint_cursor(coords: Point);
-    fn hide_cursor();
-    fn show_cursor();
+    fn paint_cursor(&self, coords: Point);
+    fn hide_cursor(&self);
+    fn show_cursor(&self);
     /// Actually move the double buffer to the single buffer and "update" the screen
-    fn draw();
-    fn clear();
+    fn draw(&self);
+    fn clear(&self);
 }
 
 #[test]
